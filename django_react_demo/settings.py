87e59f4e2c91c737fcfbcdc9950ea1d3b64ea49f
Ryan Mysliwiec <retroshade@users.noreply.github.com>
2018-04-07 12:35:08 -0400

    Removed secret key

--- django_react_demo/settings.py
+++ django_react_demo/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/2.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'zrjgr^3j3o9b@cvz$rql7-e#b+6d_^j9fk-nsj1)a#f^yy7-f('
+SECRET_KEY = # you didn't see anything
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
